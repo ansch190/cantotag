d4897b168e6e672338455fc2a34c391d6cdc3610
ansch190 <ansch190@hhu.de>
2018-02-09 21:40:23 +0100

    Initial commit

---
+++ src/main/java/Playground.java
@@ -0,0 +1,11 @@
+public class Playground {
+
+    public static void main(String[] args){
+        test0();
+    }
+
+    private static void test0(){
+        System.out.println("Hello World!");
+    }
+
+}
